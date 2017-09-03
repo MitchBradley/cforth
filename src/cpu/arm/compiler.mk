8b34bab224d5d3b8b9abb4dd6ef94e09aeec4fa7
Mitch Bradley <wmb@firmworks.com>
2017-09-02 15:12:22 -1000

    LIBGCC finder now supports paths containing spaces

--- src/cpu/arm/compiler.mk
+++ src/cpu/arm/compiler.mk
@@ -8,4 +8,21 @@ TOBJCOPY=$(CROSS)objcopy
 CPU_VARIANT ?= -marm
 TCFLAGS += $(CPU_VARIANT)
 
-LIBDIRS=-L$(dir $(shell $(TCC) $(TCFLAGS) -print-libgcc-file-name))
+LIBGCC=$(shell $(TCC) $(TCFLAGS) -print-libgcc-file-name)
+
+# This subst mess handles Windows pathnames that may contain spaces
+# Without it, the dir function can mess up badly
+
+# Tricky way to get a variable that contains a space character
+space :=
+space +=
+
+# First replace spaces with semicolons
+LS := $(subst $(space),;,$(LIBGCC))
+# Then perform dir on the space-less path
+DS := $(dir $(LS))
+# Finally restore the spaces
+LIBGCCDIR := $(subst ;,$(space),$(DS))
+
+# And quote the result to protect the spaces
+LIBDIRS:=-L"$(LIBGCCDIR)"
