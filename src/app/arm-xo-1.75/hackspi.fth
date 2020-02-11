MitchBradley/cforth

commit d642a0994dea2174c3afedd86e317c87c65e6a1c

arm-xo-1.75: conditionalize protect-fw on sec-trg-gpio#
This will allow us to support SPI on board without the OLPC
security mechanism.

src/app/arm-xo-1.75/hackspi.fth
@@ -180,9 +180,11 @@ h# 1010.0000 constant spi-mem-base
    false
 ;
 
+[ifdef] sec-trg-gpio#
 : sec-trg  ( -- )  sec-trg-gpio# gpio-set  ;
 
 : protect-fw  ( -- )  secure?  if  spi-protect sec-trg  then  ;
+[endif]
 
 \ Assumes offset is block-aligned
 : write-setup-dance  ( -- )

commit c16298a714682d036f6f544bdbc9f3f09c1c915b

arm-xo-1.75: fix OLPC build
Oops, a brain fart. Sorry.

Fixes: d642a0994dea ("arm-xo-1.75: conditionalize protect-fw on sec-trg-gpio#")

src/app/arm-xo-1.75/hackspi.fth
@@ -184,7 +184,7 @@ h# 1010.0000 constant spi-mem-base
 : sec-trg  ( -- )  sec-trg-gpio# gpio-set  ;
 
 : protect-fw  ( -- )  secure?  if  spi-protect sec-trg  then  ;
-[endif]
+[then]
 
 \ Assumes offset is block-aligned
 : write-setup-dance  ( -- )