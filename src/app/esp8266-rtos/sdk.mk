4210329daed10307311e8481131217f5ffb6a373
Mitch Bradley <wmb@firmworks.com>
2020-11-02 14:52:58 -1000

    Automated SDK and toolchain installation

--- src/app/esp8266-rtos/sdk.mk
+++ src/app/esp8266-rtos/sdk.mk
@@ -1,40 +1,61 @@
-NODEMCU_PATH ?= $(NODEMCU_PARENT_PATH)/nodemcu-firmware
-SDK_VER:=1.5.4.1
+IDF_NAME ?= ESP8266_RTOS_SDK
+IDF_PATH ?= $(IDF_PARENT_PATH)/$(IDF_NAME)
 
-XTGCCPATH ?= $(NODEMCU_PATH)/esp-open-sdk/xtensa-lx106-elf/bin/
-CROSS ?= $(XTGCCPATH)xtensa-lx106-elf-
+# This file within the SDK lists the required toolchain versions
+TOOLVERSIONS = $(IDF_PATH)/tools/toolchain_versions.mk
 
-# Include files from the SDK
-SDK_DIR:=$(NODEMCU_PATH)/sdk/esp_iot_sdk_v$(SDK_VER)
+# This is for getting a specific release version .zip file
+# IDF_VERSION ?= v3.3
+# IDF_ARCHIVE = $(IDF_NAME)-$(IDF_VERSION).zip
+# IDF_URL = $(IDF_GITUSER)/$(IDF_NAME)/releases/download/$(IDF_VERSION)/$(IDF_ARCHIVE)
+#
+# $(TOOLVERSIONS):
+# 	@echo Getting $(IDF_NAME)
+# 	(cd $(IDF_PARENT_PATH) \
+# 	&& wget $(IDF_URL) \
+# 	&& echo Unzipping $(IDF_ARCHIVE) \
+# 	&& unzip -q $(IDF_ARCHIVE) \
+# 	&& rm $(IDF_ARCHIVE) \
+# 	&& python -m pip install --user -r $(IDF_PATH)/requirements.txt \
+# 	)
 
-NODEMCU_REPO ?= https://github.com/nodemcu/nodemcu-firmware.git
-NODEMCU_COMMIT ?= 7b83bbb
-$(NODEMCU_PATH):
-	(cd $(NODEMCU_PARENT_PATH) \
-	&& git clone $(NODEMCU_REPO) \
-	&& cd $(abspath $(NODEMCU_PATH)) \
-	&& git branch cforth $(NODEMCU_COMMIT) \
-	&& git checkout cforth \
-	&& git apply --whitespace=fix $(abspath $(TOPDIR))/src/app/esp8266/*.patch \
-	&& tar -xzf tools/esp-open-sdk.tar.gz \
+# This is for cloning the lastest version from GitHub
+IDF_GITUSER ?= https://github.com/espressif
+IDF_REPO = $(IDF_GITUSER)/$(IDF_NAME).git
+
+$(TOOLVERSIONS):
+	@echo Getting $(IDF_NAME)
+	(cd $(IDF_PARENT_PATH) \
+	&& git clone $(IDF_REPO) \
+	&& python -m pip install --user -r $(IDF_PATH)/requirements.txt \
 	)
 
-$(NODEMCU_PATH)/sdk: $(NODEMCU_PATH)
-	cd $(NODEMCU_PATH) && make --no-print-directory sdk_patched
+# If the TOOLVERSIONS file is not present, make will trigger the rule to
+# make it and then restart so that its information can then be used to
+# get the right toolchain
+-include $(TOOLVERSIONS)
 
-$(PLAT_OBJS): $(NODEMCU_PATH)/sdk
+# It is hard to convert the names in toolchain_versions.mk to filenames,
+# so we hardcode it here.
+XTGCC_VERSION = gcc8_4_0-esp-2020r3
 
-BUILDDIR := $(realpath .)
+TOOLPREFIX = xtensa-lx106-elf
+XTGCC_ARCHIVE = $(TOOLPREFIX)-$(XTGCC_VERSION)-linux-amd64.tar.gz
+XTGCC_DOWNLOAD = https://dl.espressif.com/dl/$(XTGCC_ARCHIVE)
 
-0x10000.bin 0x00000.bin: $(NODEMCU_PATH)/sdk app.o
-	(cd $(NODEMCU_PATH) && PATH="${PATH}:$(XTGCCPATH)" FORTHOBJS=$(BUILDDIR)/app.o make --no-print-directory)
-	mv $(NODEMCU_PATH)/bin/*.bin .
+# Inside XTGCC_PARENT_PATH we have subdirectories for different
+# toolchain versions so it is easy to tell if we have the right
+# one, and if not, to automatically fetch it
+# Example subdirectory name: toolchain-1.22.0-80-g6c4433a-5.2.0/
+XTGCC_CONTAINER_PATH ?= $(XTGCC_PARENT_PATH)/toolchain-$(XTGCC_VERSION)
 
-# Use FS=1m FM=dout for most Sonoff devices
-FS?=32m
-FM?=dio
+XTGCCPATH ?= $(XTGCC_CONTAINER_PATH)/$(TOOLPREFIX)/bin
 
-LOADCMD=$(NODEMCU_PATH)/tools/esptool.py --port $(COMPORT) -b 115200 write_flash -fm=$(FM) -fs=$(FS) 0x00000 0x00000.bin 0x10000 0x10000.bin
-
-download: 0x00000.bin 0x10000.bin
-	$(LOADCMD)
+$(XTGCCPATH):
+	@ echo Getting toolchain
+	(mkdir -p $(XTGCC_CONTAINER_PATH) \
+	&& cd $(XTGCC_CONTAINER_PATH) \
+	&& wget $(XTGCC_DOWNLOAD) \
+	&& tar xf $(XTGCC_ARCHIVE) \
+	&& rm $(XTGCC_ARCHIVE) \
+	)
