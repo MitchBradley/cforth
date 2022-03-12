MitchBradley/cforth

commit b90399b59787d0fef9f6cd94df347a99789aeab3

Add ccalls.fth to extend.o rule
When ccalls.fth was removed by hand, make did not remake it.

Caused by ccalls.fth not known to make.

src/cforth/targets.mk
@@ -136,7 +136,7 @@ forth: $(BASEOBJS) $(HOSTOBJS)
 # and file I/O.  It is used in the compilation tools but not in the
 # embeddable version (embed.o).
 
-extend.o: $(EXTENDSRC) $(FINC) makeccalls
+extend.o ccalls.fth: $(EXTENDSRC) $(FINC) makeccalls
 	@echo CC $<
 	@$(CC) $(CFLAGS) -c $(EXTENDSRC) -o $@
 	@$(CC) $(CFLAGS) -E -C -c $(EXTENDSRC) | ./makeccalls >ccalls.fth

commit 34e7d4c053dedf0963b11515ac9d5cb849307aee

Fix extend.o rule
When ccalls.fth is missing, and make -n is used, cc would write object
to ccalls.fth, then the file would be replaced by makeccalls.

Regression introduced by
b90399b ("Add ccalls.fth to extend.o rule")

src/cforth/targets.mk
@@ -138,7 +138,7 @@ forth: $(BASEOBJS) $(HOSTOBJS)
 
 extend.o ccalls.fth: $(EXTENDSRC) $(FINC) makeccalls
 	@echo CC $<
-	@$(CC) $(CFLAGS) -c $(EXTENDSRC) -o $@
+	@$(CC) $(CFLAGS) -c $(EXTENDSRC) -o extend.o
 	@$(CC) $(CFLAGS) -E -C -c $(EXTENDSRC) | ./makeccalls >ccalls.fth
 
 # This rule builds a date stamp object that you can include in the image